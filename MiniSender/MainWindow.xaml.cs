9795f90527b3be56ae99dac205ae56510fbc3fc1
Vincent Valentine <vanilmirth@inbox.ru>
2022-07-25 15:14:53 +0300

    Не совсем корректно работающий сохранятор

--- MiniSender/MainWindow.xaml.cs
+++ MiniSender/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Mail;
 using System.Windows;
@@ -8,21 +9,19 @@ namespace MiniSender
 {  
     public partial class MainWindow : Window
     {
+        private string letterData;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+       
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                var fromAddress = new MailAddress(tbUserName.Text);
+                var fromAddress = new MailAddress(cbFromAddress.Text);
                 var toAddress = new MailAddress(tbToAddress.Text);
 
                 string subject = tbSubject.Text;
@@ -48,14 +47,34 @@ namespace MiniSender
                 })
                 {
                     smtp.Send(message);
+                    tbLog.Text = "Message has been sent";
+
                 }
-                MessageBox.Show("Message has been sent");
+                //MessageBox.Show("Message has been sent");
+
             }
             catch (Exception ex)
             {
                 Debug.Write(ex.ToString());
                 Console.WriteLine(ex);
             }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"E:\УЧЁБА\logs\mail.txt";
+            if (!File.Exists(path))
+            {
+                letterData = "От кого: " + tbUserName.Text + "\n" + "Кому: " + tbToAddress.Text + "\n" + tbSubject.Text + "\n" + tbBody.Text + Environment.NewLine;
+                File.WriteAllText(path, letterData);
+            }
+            File.AppendAllText(path, letterData);
+
         }
     }
 }

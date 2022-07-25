11f4c201c2283fc724cff53d225767334758ba8d
Vincent Valentine <vanilmirth@inbox.ru>
2022-07-25 14:45:11 +0300

    Работающий почтовик

--- MiniSender/MainWindow.xaml.cs
+++ MiniSender/MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MiniSender
-{
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+{  
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,6 +16,46 @@ namespace MiniSender
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var fromAddress = new MailAddress(tbUserName.Text);
+                var toAddress = new MailAddress(tbToAddress.Text);
+
+                string subject = tbSubject.Text;
+
+                string body = tbBody.Text;
+
+                string password = tbPassword.Password;
+
+                var smtp = new SmtpClient()
+
+                {
+                    Host = tbServer.Text,
+                    Port = Int32.Parse(tbPort.Text),
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(tbUserName.Text, password),
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = body
+                })
+                {
+                    smtp.Send(message);
+                }
+                MessageBox.Show("Message has been sent");
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.ToString());
+                Console.WriteLine(ex);
+            }
         }
     }
 }

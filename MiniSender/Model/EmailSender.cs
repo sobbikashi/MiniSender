d28ba387e7ac23cb6dd7c0aa1ae6cc9086bea9de
Vincent Valentine <vanilmirth@inbox.ru>
2022-08-01 15:25:47 +0300

    франкенштейн

---
+++ MiniSender/Model/EmailSender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Mail;
+using MiniSender;
+
+namespace MiniSender.Model
+{
+    class EmailSender
+    {
+        public void Send(MailAddress fromAddress, MailAddress toAddress, string message, string password)
+        {
+            try
+            {
+                fromAddress = new MailAddress("test.send207@gmail.com");
+                toAddress = new MailAddress("vanilmirth@inbox.ru");
+
+                //string subject = message.Subject;
+
+                //string body = message.Body;
+
+                //string password = tbPassword.Password;
+
+                var smtp = new SmtpClient()
+
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("test.send207@gmail.com", password),
+                };                
+                    smtp.Send(fromAddress, toAddress, message, password);
+                    //MiniSender.tbLog.Text = DateTime.Now.ToString() + " Message has been sent";
+                                
+                //MessageBox.Show("Message has been sent");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.ToString());
+                Console.WriteLine(ex.ToString());
+                //tbLog.Text = DateTime.Now.ToString() + " " + ex.ToString();
+            }
+        }
+
+    }
+}
---
+++ MiniSender/View/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace MiniSender
+{  
+    public partial class MainWindow : Window
+    {
+        private string letterData;
+        private MailAddress from;
+        private MailAddress to;
+
+     
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            Model.EmailSender emailSender = new Model.EmailSender();
+            MailMessage mail = new MailMessage();
+            mail.Subject = tbSubject.Text;
+            mail.Body = tbBody.Text;
+            from = new MailAddress("test.send207 @gmail.com");
+            to = new MailAddress("vanilmirth@inbox.ru");
+            emailSender.Send(from, to, mail, tbPassword.Password);
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"E:\УЧЁБА\logs\mail.txt";
+            if (!File.Exists(path))
+            {
+                letterData = "От кого: " + tbUserName.Text + "\n" + "Кому: " + tbToAddress.Text + "\n" + tbSubject.Text + "\n" + tbBody.Text + Environment.NewLine;
+                File.WriteAllText(path, letterData);
+            }
+            File.AppendAllText(path, letterData);
+
+        }
+    }
+}
